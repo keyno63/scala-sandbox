5cd5733ea62de32c16fc371a563bf421a9272c49
Keyno <kofujiw@gmail.com>
2019-08-05 23:41:55 +0900

    add capture 1.

---
+++ src/main/scala/jp/co/who/cop/c1/C1.scala
@@ -0,0 +1,34 @@
+package jp.co.who.cop.c1
+
+import java.math.BigInteger
+
+//import org.scalatest._
+
+class C1 {
+
+  def cap_1_1_0() = {
+    var capital = Map("Map" -> "Washington", "France" -> "Paris")
+    capital += ("Japan" -> "Tokyo")
+    println(capital("France"))
+  }
+
+  def cap_1_1_1() = {
+    def factorial(x: BigInteger): BigInteger = {
+      if (x == BigInteger.ZERO)
+        BigInteger.ONE
+      else
+        x.multiply(factorial(x.subtract(BigInteger.ONE)))
+    }
+  }
+
+  def cap_1_1_2() = {
+
+  }
+
+  def cap_1_2_2() = {
+    val xs = 1 to 3
+    val it = xs.iterator
+    //eventually { it.next() shouldBe 3 }
+  }
+
+}
