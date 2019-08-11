b724d0cd1b3b136479dc0f6e5106876c6ebba970
Keyno <kofujiw@gmail.com>
2019-08-11 23:36:40 +0900

    implementation cap 4.

---
+++ src/main/scala/jp/co/who/cop/c4/C4.scala
@@ -0,0 +1,45 @@
+package jp.co.who.cop.c4
+
+class C4 {
+  class ChecksumAccumulator {
+    var sum = 0
+    def add(b: Byte): Unit = { sum += b }
+    def uncheck(): Int = ~(sum & 0xFF) + 1
+  }
+
+  def cap_4_1_0() = {
+    val acc = new ChecksumAccumulator
+    val csa = new ChecksumAccumulator
+    println(acc.sum, csa.sum)
+    acc.sum = 3
+    println(acc.sum, csa.sum)
+  }
+
+  import scala.collection.mutable
+  object ChecksumAccumulator {
+    private val cache = mutable.Map.empty[String, Int]
+    def calculate(s: String): Int =
+      if (cache.contains(s))
+        cache(s)
+      else {
+        val acc = new ChecksumAccumulator
+        for (c <- s)
+          acc.add(c.toByte)
+        val cs = acc.uncheck()
+        cache += (s -> cs)
+        cs
+      }
+  }
+  def cap_4_3_0() = {
+    val s = "Every value is an object"
+    val ret = ChecksumAccumulator.calculate(s)
+    println(ret)
+  }
+
+}
+
+object C4 extends App {
+  val c4 = new C4
+  c4.cap_4_1_0()
+  c4.cap_4_3_0()
+}
