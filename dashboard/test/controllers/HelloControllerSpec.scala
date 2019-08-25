885907fb53dffa57ca3f2598058c9c8beb56064e
Keyno <kofujiw@gmail.com>
2019-08-25 22:44:43 +0900

    dashboard, add test pattern.

--- dashboard/test/controllers/HelloControllerSpec.scala
+++ dashboard/test/controllers/HelloControllerSpec.scala
@@ -8,7 +8,7 @@ class HelloControllerSpec extends AirSpec {
 
   val controller = new HelloController(stubControllerComponents())
 
-  def `root_クエリーパラメータがある場合は「Hello, namae!」というレスポンスを返す`(): Unit = {
+  def `root_クエリーパラメーターがある場合は「Hello, namae!」というレスポンスを返す`(): Unit = {
     val name = "namae"
     val result = controller.get(Some(name))(FakeRequest())
 
@@ -16,14 +16,14 @@ class HelloControllerSpec extends AirSpec {
     contentAsString(result) shouldBe s"Hello, $name!"
   }
 
-  def `root_クエリーパラメータがない場合は「Please give a name as a query parameter named "name".」というレスポンスを返す`():Unit = {
+  def `root_クエリーパラメーターがない場合は「Please give a name as a query parameter named "name".」というレスポンスを返す`():Unit = {
     val result = controller.get(None)(FakeRequest())
 
     status(result) shouldBe 200
     contentAsString(result) shouldBe """Please give a name as a query parameter named "name"."""
   }
 
-  def `クエリーパラメータが指定されている場合は合計値のレスポンスを返す"`(): Unit = {
+  def `クエリーパラメーターが指定されている場合は合計値のレスポンスを返す"`(): Unit = {
     val seq = Seq(1, 2)
     val result = controller.plus(Some(seq.head), Some(seq(1)))(FakeRequest(GET, "/plus"))
 
@@ -31,10 +31,24 @@ class HelloControllerSpec extends AirSpec {
     contentAsString(result) shouldBe seq.sum.toString
   }
 
-  def `クエリーパラメータがない場合は 0 のレスポンスを返す`(): Unit = {
+  def `クエリーパラメーターがない場合は 0 のレスポンスを返す`(): Unit = {
     val result =  controller.plus(None, None)(FakeRequest(GET, "/plus"))
 
     status(result) shouldBe 200
     contentAsString(result) shouldBe 0.toString
+  }
+
+  def `クエリーパラメーターが足りない場合はもらった値をレスポンスを返す`(): Unit = {
+    val result =  controller.plus(None, Some(1))(FakeRequest(GET, "/plus"))
+
+    status(result) shouldBe 200
+    contentAsString(result) shouldBe 1.toString
+  }
+
+  def `クエリーパラメーターが足りない場合はもらった値をレスポンスを返す, その2`(): Unit = {
+    val result =  controller.plus(Some(2), None)(FakeRequest(GET, "/plus"))
+
+    status(result) shouldBe 200
+    contentAsString(result) shouldBe 2.toString
   }
 }
