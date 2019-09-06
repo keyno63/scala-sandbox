a7ff2ff8269d841f31275bc3d66c336b6d767242
Keyno <kofujiw@gmail.com>
2019-09-07 00:03:12 +0900

    GoF. Facade.

--- gof/src/main/scala/jp/co/who/gof/facade/Item.scala
+++ gof/src/main/scala/jp/co/who/gof/facade/Item.scala
@@ -1,14 +1,33 @@
 package jp.co.who.gof.facade
 
+import scala.collection.mutable
+
 case class Item(name: String, price: Int)
+case class Order(id: Int) {
+  private var list = List[Item]()
+  def addItem(item: Item): Unit = {
+    list :+= item
+  }
+
+  def removeItem(item: Item): Unit = ???
+}
 
 case class Shop[T <: Item](name: String) {
   private var list = List[T]()
-  def addItem(item: Item): Unit = {
+  def addItem(item: T): Unit = {
     list :+= item
   }
 }
 
+
 object OrderSystem {
+
+  private var orders: mutable.HashMap[Int, Order] = new mutable.HashMap[Int, Order]
+  def addOrder(order: Order): Unit = {
+    this.orders ++= mutable.HashMap(order.id -> order)
+  }
+  def getOrder(id: Int): Order = {
+    this.orders(id)
+  }
 
 }
