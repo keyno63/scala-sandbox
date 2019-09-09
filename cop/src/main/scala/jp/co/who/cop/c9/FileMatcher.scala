9f5318a4250c94d68eef035e86ef65aaf741a0fa
Keyno <kofujiw@gmail.com>
2019-09-09 23:52:44 +0900

    add education code, functional method.

---
+++ cop/src/main/scala/jp/co/who/cop/c9/FileMatcher.scala
@@ -0,0 +1,19 @@
+package jp.co.who.cop.c9
+
+import java.io.File
+
+object FileMatcher {
+  private def fileHere: Array[File]  = new java.io.File(".").listFiles
+  def fileEnding(query: String) =
+    for (file <- fileHere; if file.getName.endsWith(query))
+      yield file
+  def fileEnding1(query: String): Array[File] =
+    fileHere.filter(_.getName.endsWith(query))
+
+  def fileContaining(query: String): Array[File] =
+    fileHere.filter(_.getName.contains(query))
+
+  def fileRegex(query: String): Array[File] =
+    fileHere.filter(_.getName.matches(query))
+
+}
