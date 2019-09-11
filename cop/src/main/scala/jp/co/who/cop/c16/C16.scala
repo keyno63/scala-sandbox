a474b39e678f5907463dc759beee93a191181a3a
Keyno <kofujiw@gmail.com>
2019-09-11 23:59:11 +0900

    add education code.

--- cop/src/main/scala/jp/co/who/cop/c16/C16.scala
+++ cop/src/main/scala/jp/co/who/cop/c16/C16.scala
@@ -1,8 +1,14 @@
 package jp.co.who.cop.c16
 
-class C16 {
+object C16 {
   val fruit = List("apples", "oranges", "pears")
 
+  def main(args: Array[String]): Unit = {
+    val l = ControllList.isort(List(10, 1, 2, 3))
+  }
+}
+
+object ControllList {
   def isort(xs: List[Int]): List[Int] =
     if (xs.isEmpty) Nil
     else insert(xs.head, isort(xs.tail))
@@ -10,5 +16,4 @@ class C16 {
   def insert(x: Int, xs: List[Int]): List[Int] =
     if (xs.isEmpty || x <= xs.head) x::xs
     else xs.head :: insert(x, xs.tail)
-
 }
