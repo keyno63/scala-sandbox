f9dfcc9b430b794b7f8f0c8aefe44bfe95e14701
Keyno <kofujiw@gmail.com>
2019-09-27 01:56:28 +0900

    add GCP Client, Cloud Strage.

---
+++ gcpclient/src/main/scala/jp/co/who/gcpclient/config/Config.scala
@@ -0,0 +1,6 @@
+package jp.co.who.gcpclient.config
+
+trait Config {
+  val BUCKET = "kof-gae-project.appspot.com"
+  val PREFIX = "sample_dir/"
+}
---
+++ gcpclient/src/main/scala/jp/co/who/gcpclient/csc/SampleCloudStrageClient.scala
@@ -0,0 +1,32 @@
+package jp.co.who.gcpclient.csc
+
+import com.google.auth.oauth2.ServiceAccountCredentials
+import com.google.cloud.storage.Storage
+import com.google.cloud.storage.StorageOptions
+import java.io._
+
+import jp.co.who.gcpclient.config.Config
+
+object SampleCloudStrageClient
+  extends Config with App {
+
+  @throws[IOException]
+  override def main(args: Array[String]): Unit = {
+    val storage = if (args.length > 0) getStorageFromJsonKey(args(0))
+    else StorageOptions.getDefaultInstance.getService
+    val bucket = storage.get(BUCKET)
+    // 特定のディレクトリのみに絞る
+    val option = Storage.BlobListOption.prefix(PREFIX)
+    val blobs = bucket.list(option)
+    val blobIterator = blobs.iterateAll
+    blobIterator.forEach(x => println(x.getName))
+  }
+
+  @throws[IOException]
+  private def getStorageFromJsonKey(key: String): Storage =
+    StorageOptions
+      .newBuilder
+      .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream(key)))
+      .build
+      .getService
+}
