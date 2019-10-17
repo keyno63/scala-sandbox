031008ab30b209e752c8482e6854961c1fecb5c4
Keyno <kofujiw@gmail.com>
2019-10-17 23:51:06 +0900

    add sealed trait reason.

---
+++ dashboard/app/domain/FailureResponse.scala
@@ -0,0 +1,6 @@
+package domain
+
+sealed trait FailureResponse
+case class FailedParseJson(reason: String) extends FailureResponse
+case class FailedHttpRequest(reason: String) extends FailureResponse
+case class FailedParams(reason: String) extends FailureResponse
