0ebdf06381f2208e54dfa2cb2cdf3bed609ffeb3
Keyno <kofujiw@gmail.com>
2019-12-24 23:50:39 +0900

    update db settings.

--- dashboard/app/controllers/DBController.scala
+++ dashboard/app/controllers/DBController.scala
@@ -56,11 +56,4 @@ class DBController @Inject()(cc: ControllerComponents, override val messagesApi:
     )
   }
 
-  def get2(): Action[AnyContent] = Action { implicit request =>
-    Ok("")
-  }
-
-  def post2(): Action[AnyContent] = Action { implicit request =>
-    Ok("")
-  }
 }
