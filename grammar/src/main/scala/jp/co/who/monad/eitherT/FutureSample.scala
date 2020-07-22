0eb5fa4ba2f5300a25d395dbe48e92fce2eb6d7b
Keyno <kofujiw@gmail.com>
2020-07-23 04:36:29 +0900

    sample feature

---
+++ grammar/src/main/scala/jp/co/who/monad/eitherT/FutureSample.scala
@@ -0,0 +1,30 @@
+package jp.co.who.monad.eitherT
+
+import scala.util._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.io.Source
+
+object FutureSample extends App {
+
+  val url = "http://sample.com/"
+  val f = Future(HttpTextClient.get(url))
+
+  f.onComplete{
+    case Success(body) =>
+      println("success")
+      println(body.mkString)
+      body.close
+    case Failure(t) => t.printStackTrace()
+  }
+  Await.ready(f, duration.Duration.Inf)
+
+}
+
+import scala.io._
+object HttpTextClient {
+  def get(url: String): BufferedSource = {
+    Source.fromURL(url)
+  }
+}
