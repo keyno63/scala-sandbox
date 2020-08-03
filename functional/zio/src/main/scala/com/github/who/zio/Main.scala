97fbe0e7627e70693504738c5bc438bece2bc80a
Keyno <kofujiw@gmail.com>
2020-08-03 22:05:27 +0900

    impl sandbox of zio code

--- functional/zio/src/main/scala/com/github/who/zio/Main.scala
+++ functional/zio/src/main/scala/com/github/who/zio/Main.scala
@@ -2,13 +2,18 @@ package com.github.who.zio
 
 import java.io.IOException
 
-import zio.{ExitCode, URIO, ZIO}
+import zio.{ExitCode, URIO, ZIO, Task}
 import zio.console._
 
 // https://zio.dev/docs/getting_started.html
 object Main extends zio.App {
-  def run(args: List[String]): URIO[Console, ExitCode] =
-    myAppLogic.exitCode
+  def run(args: List[String]): URIO[Console, ExitCode] = {
+    //myAppLogic.exitCode
+    Task(User(1, "name"))
+      //.foldCause(_ => User(0, "none"), _)
+      .exitCode
+    myEitherLogic.exitCode
+  }
 
   val myAppLogic: ZIO[Console, IOException, Unit] =
     for {
@@ -16,4 +21,24 @@ object Main extends zio.App {
       name <- getStrLn
       _    <- putStrLn(s"Hello, ${name}, welcome to ZIO!")
     } yield ()
+
+  val myEitherLogic: ZIO[Console, Exception, Unit] = {
+    for {
+      _    <- putStrLn("Hello! What is your name?")
+      name <- getStrLn
+      ret <- ZIO.fromEither(func1(name))
+      //retStr <- ret.flatMap(s => s"input: $s")
+      _    <- putStrLn(s"Hello, ${name}, $ret welcome to ZIO!")
+    } yield ()
+  }
+
+
+  def func1(name: String): Either[Exception, String] = {
+    name match {
+      case "hoge" => Left(new Exception("failed"))
+      case _ => Right(name)
+    }
+  }
 }
+
+case class User(id: Int, name: String)
