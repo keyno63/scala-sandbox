d1c3656fd19e22cf003da229cf5b520885c45ec7
Keyno <kofujiw@gmail.com>
2020-08-06 04:02:10 +0900

    fmt for http4s sample code

--- http/http4s-sample/src/main/scala/com/github/keyno/http4s/ExampleApp.scala
+++ http/http4s-sample/src/main/scala/com/github/keyno/http4s/ExampleApp.scala
@@ -1,39 +1,36 @@
 package com.github.keyno.http4s
 
-import cats.effect.{ConcurrentEffect, ContextShift, ExitCode, IO, IOApp, Timer}
+import cats.effect.{ ConcurrentEffect, ContextShift, ExitCode, IO, IOApp, Timer }
 import cats.implicits._
 import fs2.Stream
 import org.http4s.server.blaze.BlazeServerBuilder
 
 import scala.concurrent.ExecutionContext.global
 import cats.effect.Sync
-import org.http4s.{HttpRoutes, MediaType}
+import org.http4s.{ HttpRoutes, MediaType }
 import org.http4s.dsl.Http4sDsl
 import org.http4s.server.middleware.Logger
 import org.http4s.implicits._
 import org.log4s.getLogger
 
-
 object ExampleApp extends IOApp {
   val logger = getLogger(this.getClass)
-  val host = "0.0.0.0"
-  val port = 8080
+  val host   = "0.0.0.0"
+  val port   = 8080
 
-  def run(args: List[String]) = {
+  def run(args: List[String]) =
     stream[IO].compile.drain.as(ExitCode.Success)
-  }
-
 
   def stream[F[_]: ConcurrentEffect](implicit T: Timer[F], C: ContextShift[F]): Stream[F, Nothing] = {
 
-    val httpApp = (Route.echoRoutes[F]()).orNotFound
+    val httpApp      = (Route.echoRoutes[F]()).orNotFound
     val finalHttpApp = Logger.httpApp(true, true)(httpApp)
 
     for {
       exitCode <- BlazeServerBuilder[F](global)
-        .bindHttp(port, host)
-        .withHttpApp(finalHttpApp)
-        .serve
+                   .bindHttp(port, host)
+                   .withHttpApp(finalHttpApp)
+                   .serve
     } yield exitCode
   }.drain
 
@@ -43,32 +40,32 @@ object Route {
   val `application/graphql` = new MediaType("application", "graphql")
 
   def helloWorldRoutes[F[_]: Sync](): HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F]{}
+    val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] {
       case GET -> Root / "hello" / name =>
         for {
           greeting <- name.pure[F]
-          resp <- Ok(greeting)
+          resp     <- Ok(greeting)
         } yield resp
     }
   }
 
   def echoRoutes[F[_]: Sync](): HttpRoutes[F] = {
-    val dsl = new Http4sDsl[F]{}
+    val dsl = new Http4sDsl[F] {}
     import dsl._
     HttpRoutes.of[F] {
       case req @ POST -> Root / "echo" =>
         if (req.contentType.map(_.mediaType).contains(`application/graphql`))
-        for {
-          greeting <- req.bodyText.pure[F]
-          //ret = Stream.eval("graphql") <+> greeting
-          resp <- Ok(greeting)
-        } yield resp
+          for {
+            greeting <- req.bodyText.pure[F]
+            //ret = Stream.eval("graphql") <+> greeting
+            resp <- Ok(greeting)
+          } yield resp
         else
           for {
             greeting <- req.bodyText.pure[F]
-            resp <- Ok(greeting )
+            resp     <- Ok(greeting)
           } yield resp
     }
   }
@@ -80,5 +77,5 @@ object Route {
       ("message", Json.fromString(name)),
     )
   }
-   */
+ */
 }
