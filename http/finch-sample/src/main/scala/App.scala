1cd572e590d9e4050455682c33f0730987801c9b
Keyno <kofujiw@gmail.com>
2020-08-06 22:48:22 +0900

    fix lint

--- http/finch-sample/src/main/scala/App.scala
+++ http/finch-sample/src/main/scala/App.scala
@@ -1,9 +1,9 @@
 import io.finch._
-import cats.effect.{IO, Sync}
-import com.twitter.finagle.{Http, ListeningServer}
+import cats.effect.{ IO, Sync }
+import com.twitter.finagle.{ Http, ListeningServer }
 import com.twitter.util.Await
 import io.finch.DecodeEntity.decodeString
-import shapeless.{:+:, CNil}
+import shapeless.{ :+:, CNil }
 
 object App extends scala.App with Endpoint.Module[IO] {
   val endpoint = "api" :: "sample" :: Service.makeHttpService()
@@ -26,24 +26,23 @@ object Service extends Endpoint.Module[IO] {
         case Some(id) =>
           UserAccountRepository.find(id) match {
             case Some(acount) => Ok(acount.toString)
-            case _ => NotFound(new Exception(s"not found account, id: $id"))
+            case _            => NotFound(new Exception(s"not found account, id: $id"))
           }
         case _ => BadRequest(new Exception("query is invalid"))
       }
-    } :+: post(stringBody:: header("content-type") ) {
-      (body: String, header: String) =>
-        (body, header) match {
-          case (_, "application/json") =>
-            Ok(s"Json request succeed to json $body")
-          case (_, "application/x-www-form-urlencoded") =>
-            Ok(s"UrlEncode request succeed to json $body")
-          case _ => BadRequest(new Exception("request is invalid"))
-        }
+    } :+: post(stringBody :: header("content-type")) { (body: String, header: String) =>
+      (body, header) match {
+        case (_, "application/json") =>
+          Ok(s"Json request succeed to json $body")
+        case (_, "application/x-www-form-urlencoded") =>
+          Ok(s"UrlEncode request succeed to json $body")
+        case _ => BadRequest(new Exception("request is invalid"))
+      }
     }
 
   private val query =
     paramOption[String]("id")
-    .mapAsync(query => IO.apply(query))
+      .mapAsync(query => IO.apply(query))
 }
 
 case class UserAccount(id: String, name: String) {
@@ -54,18 +53,16 @@ object UserAccountRepository {
   private val userAccountList = List(
     UserAccount("0", "Alice"),
     UserAccount("1", "Bob"),
-    UserAccount("2", "Crith"),
+    UserAccount("2", "Crith")
   )
 
-  def find(id: String): Option[UserAccount] = {
+  def find(id: String): Option[UserAccount] =
     userAccountList.filter(_.id == id).head match {
       case ret @ UserAccount(_, _) => Some(ret)
-      case _ => None
+      case _                       => None
     }
-  }
 
-  def update(account: UserAccount): Either[Throwable, UserAccount] = {
+  def update(account: UserAccount): Either[Throwable, UserAccount] =
     // sample なので常に代入する Right を返す.
     Right(account)
-  }
 }
