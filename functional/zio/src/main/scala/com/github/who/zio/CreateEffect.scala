68e2ebd2f8a1840dbc10631b62ccb4836a5268dd
Keyno <kofujiw@gmail.com>
2020-08-18 00:43:10 +0900

    zio effect sample, sprit

--- functional/zio/src/main/scala/com/github/who/zio/CreateEffect.scala
+++ functional/zio/src/main/scala/com/github/who/zio/CreateEffect.scala
@@ -1,29 +1,20 @@
 package com.github.who.zio
 
-import com.github.who.zio.Main.unsafeRun
+import com.github.who.zio.CreateEffect.{ teamId, zioOption }
 import zio.{ IO, ZIO }
 
-object CreateEffect extends App {
+case class EffectUser(id: String, name: String, teamId: teamId)
+case class Team(id: String, name: String)
 
-  val ids = List("id1", "id2", "nothing")
-  for {
-    v <- ids
-    r = zioOption(v)
-  } println(
-    unsafeRun(
-      r.catchAll(e => ZIO.succeed(e))
-    )
-  )
+object CreateEffect {
+  type teamId = String
 
-  def zioOption(id: String) = {
+  def zioOption(id: String): IO[Throwable, Option[(EffectUser, Team)]] = {
     // setUp
-    type teamId = String
-    case class User(id: String, name: String, teamId: teamId)
-    case class Team(id: String, name: String)
     val users = List(
-      User("id1", "Alice", "t1"),
-      User("id2", "Bob", "t3"),
-      User("id3", "Crith", "t2")
+      EffectUser("id1", "Alice", "t1"),
+      EffectUser("id2", "Bob", "t3"),
+      EffectUser("id3", "Crith", "t2")
     )
     val teams = List(
       Team("t1", "Alice1"),
@@ -31,7 +22,7 @@ object CreateEffect extends App {
     )
 
     val maybeId: IO[Option[Nothing], String] = ZIO.fromOption(Some(id))
-    def getUser(userId: String): IO[Throwable, Option[User]] =
+    def getUser(userId: String): IO[Throwable, Option[EffectUser]] =
       users.find(_.id == userId) match {
         case u @ Some(_) => ZIO.succeed(u)
         case _           => ZIO.fail(new Exception(s"not found user. id: $userId"))
@@ -42,11 +33,21 @@ object CreateEffect extends App {
         case _       => ZIO.fail(new Exception(s"not found team. id: $teamId"))
       }
 
-    val result: IO[Throwable, Option[(User, Team)]] = (for {
+    val result: IO[Throwable, Option[(EffectUser, Team)]] = (for {
       id   <- maybeId
       user <- getUser(id).some
       team <- getTeam(user.teamId).asSomeError
     } yield (user, team)).optional
     result
   }
+}
+
+object CreateSample extends zio.BootstrapRuntime with scala.App {
+    val ids = List("id1", "id2", "nothing")
+    for {
+      v <- ids
+      r = zioOption(v)
+    } println(unsafeRun(
+        r.catchAll(e => ZIO.succeed(e))
+      ))
 }
