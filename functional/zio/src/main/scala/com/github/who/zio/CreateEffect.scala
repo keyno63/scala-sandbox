75d9f8b23ffca225831d84e4871daa32cd7db40b
Keyno <kofujiw@gmail.com>
2020-08-18 00:25:06 +0900

    zio effect sample

--- functional/zio/src/main/scala/com/github/who/zio/CreateEffect.scala
+++ functional/zio/src/main/scala/com/github/who/zio/CreateEffect.scala
@@ -1,16 +1,19 @@
 package com.github.who.zio
 
-import zio.{ExitCode, IO, Task, UIO, URIO, ZIO}
-import zio.console.Console
+import com.github.who.zio.Main.unsafeRun
+import zio.{ IO, ZIO }
 
 object CreateEffect extends App {
 
-  val ids = List("id1", "nothing")
+  val ids = List("id1", "id2", "nothing")
   for {
     v <- ids
     r = zioOption(v)
-    _ <-
-  } r
+  } println(
+    unsafeRun(
+      r.catchAll(e => ZIO.succeed(e))
+    )
+  )
 
   def zioOption(id: String) = {
     // setUp
@@ -19,7 +22,7 @@ object CreateEffect extends App {
     case class Team(id: String, name: String)
     val users = List(
       User("id1", "Alice", "t1"),
-      User("id2", "Bob", "t1"),
+      User("id2", "Bob", "t3"),
       User("id3", "Crith", "t2")
     )
     val teams = List(
@@ -29,19 +32,15 @@ object CreateEffect extends App {
 
     val maybeId: IO[Option[Nothing], String] = ZIO.fromOption(Some(id))
     def getUser(userId: String): IO[Throwable, Option[User]] =
-      users
-        .filter(_.id == userId)
-        .head match {
-        case u @ User(_, _, _) => ZIO.succeed(Some(u))
-        case _ =>  ZIO.fail(new Exception(s"not found. id: $userId"))
+      users.find(_.id == userId) match {
+        case u @ Some(_) => ZIO.succeed(u)
+        case _           => ZIO.fail(new Exception(s"not found user. id: $userId"))
       }
     def getTeam(teamId: String): IO[Throwable, Team] =
-      teams
-      .filter(_.id == teamId)
-      .head match {
-      case t @ Team(_, _) => ZIO.succeed(t)
-      case _ =>  ZIO.fail(new Exception(s"not found. id: $teamId"))
-    }
+      teams.find(_.id == teamId) match {
+        case Some(t) => ZIO.succeed(t)
+        case _       => ZIO.fail(new Exception(s"not found team. id: $teamId"))
+      }
 
     val result: IO[Throwable, Option[(User, Team)]] = (for {
       id   <- maybeId
