1c538356789ef73d4ed8353d1319fe480416a139
Keyno <kofujiw@gmail.com>
2020-08-25 22:29:30 +0900

    add type safe config

--- tools/awsclient/src/main/scala/AwsSample.scala
+++ tools/awsclient/src/main/scala/AwsSample.scala
@@ -1,11 +1,16 @@
 import client.S3Client
+import com.typesafe.config._
 
 object AwsSample extends scala.App {
 
-  val accessKey = ???
-  val secretKey = ???
+  // typesafe config
+  val config = ConfigFactory.load()
+
+  val accessKey = config.getString("aws.s3.access-key")
+  val secretKey = config.getString("aws.s3.secret-key")
   val region = "us-east-2"
   val bucketName = "kofujiw-s3bucket"
+  println(accessKey + secretKey)
   val s3 = S3Client.s3Client(accessKey, secretKey, region, bucketName)
   println(s3.getBuckets)
 
