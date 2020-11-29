911b9fe71d6ec67bc4d97807eefc5954a1eb7883
Keyno <kofujiw@gmail.com>
2020-11-29 13:55:46 +0900

    get value from setting config(application.conf)

--- http/skinny-sample/src/main/scala/com/github/keyno/skinny/ExampleApp.scala
+++ http/skinny-sample/src/main/scala/com/github/keyno/skinny/ExampleApp.scala
@@ -1,14 +1,20 @@
 package com.github.keyno.skinny
 
-import skinny.http.{ HTTP, Request }
+import skinny.http.{HTTP, Request}
+import com.typesafe.config._
 
 object ExampleApp extends scala.App {
 
+  // setting config.
+  val config = ConfigFactory.load()
+  val url = config.getString("app.url")
+  val connectionTimeout = config.getInt("app.connectionTimeOut")
+  val readTimeout = config.getInt("app.readTimeOut")
+
   // 自前の mock server 用のリクエスト先.
-  val url     = "http://localhost:8080/api/webclient/test3"
   val request = new Request(url)
-  request.connectTimeoutMillis(5000)
-  request.readTimeoutMillis(5000)
+  request.connectTimeoutMillis(connectionTimeout)
+  request.readTimeoutMillis(readTimeout)
 
   // 自前の mock server 用の body.
   val body =
