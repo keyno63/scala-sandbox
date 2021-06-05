d6f5142032b2d547c316cc3eaabd60e3f47ae68e
Keyno <kofujiw@gmail.com>
2021-06-06 05:03:03 +0900

    circe のParser/Decoder の動作確認をテストに書いた

--- json/circe-sample/src/test/scala/CirceParsingSuite.scala
+++ json/circe-sample/src/test/scala/CirceParsingSuite.scala
@@ -1,4 +1,9 @@
-import io.circe.ParsingFailure
+import io.circe.{ Json, ParsingFailure }
+// 自動で変換したい場合
+//import io.circe.generic.auto._
+// Encoder/Decoder を自作したい場合
+import io.circe.{ Decoder, Encoder }
+import io.circe.syntax._
 import org.scalatest.flatspec._
 
 class CirceParsingSuite extends AnyFlatSpec {
@@ -38,5 +43,21 @@ class CirceParsingSuite extends AnyFlatSpec {
 //        Left(ParsingFailure("exhausted input", new Exception()))
 //    )
   }
+
+  "jsonObj" should "create" in {
+    val field = Some("hoge").map(value => "id" -> Json.fromString(value)) ++
+      Some("name_test_value").map(value => "name_value" -> Json.fromString(value))
+    val jsonValue = Json
+      .fromFields(field)
+      .as[SomeOne]
+    val expected = SomeOne("hoge", "name_test_value")
+    assert(jsonValue == Right(expected))
+  }
+
+  case class SomeOne(id: String, name: String)
+  implicit val decoderSome: Decoder[SomeOne] =
+    Decoder.forProduct2("id", "name_value")(SomeOne)
+  implicit val encoderSome: Encoder[SomeOne] =
+    Encoder.forProduct2("id", "name_value")(someOne => (someOne.id, someOne.name))
 
 }
