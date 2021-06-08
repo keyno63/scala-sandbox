38d08afef634befc788342425cbc8b6fea75395f
Keyno <kofujiw@gmail.com>
2021-06-08 09:19:16 +0900

    spray-json, 親オブジェクトのJsonParser実装

--- json/spray-json-sample/src/main/scala/com/github/keyno/sprayjson/domain/GraphQLRequest.scala
+++ json/spray-json-sample/src/main/scala/com/github/keyno/sprayjson/domain/GraphQLRequest.scala
@@ -1,8 +1,26 @@
 package com.github.keyno.sprayjson.domain
 
+import spray.json.{ enrichAny, JsArray, JsString, JsValue, RootJsonFormat }
+
 case class GraphQLRequest(
   query: Option[String] = None,
   operationName: Option[String] = None,
   variable: Option[Map[String, Other]] = None,
   extensions: Option[Map[String, Other]] = None
 )
+
+object GraphQLRequest {
+  import com.github.keyno.sprayjson.domain.Other._
+
+  implicit object sprayJsonFormat extends RootJsonFormat[GraphQLRequest] {
+    override def write(obj: GraphQLRequest): JsValue =
+      JsArray(
+        JsString(obj.query.getOrElse("")),
+        JsString(obj.operationName.getOrElse("")),
+        obj.variable.getOrElse(Map.empty).toJson,
+        obj.extensions.getOrElse(Map.empty).toJson
+      )
+
+    override def read(json: JsValue): GraphQLRequest = json.convertTo[GraphQLRequest](sprayJsonFormat)
+  }
+}
---
+++ json/spray-json-sample/src/test/scala/com/github/keyno/sprayjson/domain/GraphQLRequestTest.scala
@@ -0,0 +1,36 @@
+package com.github.keyno.sprayjson.domain
+
+import com.github.keyno.sprayjson.domain.Other._
+import org.scalatest.flatspec.AnyFlatSpec
+import spray.json.{ enrichAny, JsArray, JsString, JsValue }
+
+class GraphQLRequestTest extends AnyFlatSpec {
+  import com.github.keyno.sprayjson.domain.GraphQLRequest._
+
+  "graphql request class" should "convert to JsonObj and JsonString" in {
+    val gql1: GraphQLRequest = GraphQLRequest(
+      query = Some("queryValue"),
+      operationName = Some("operationValue"),
+      variable = Some(Map("key1" -> StringValue("value1"))),
+      extensions = Some(
+        Map(
+          "key2" ->
+            ListValue(List(StringValue("lValue1"), StringValue("lValue2")))
+        )
+      )
+    )
+    val ret1: JsValue = gql1.toJson
+//    val expected1: JsValue = JsArray(
+//      JsString("queryValue"),
+//      JsString("operationValue"),
+//      Map("key1" -> StringValue("value1")).toJson(mapFormat(StringJsonFormat, sprayJsonOtherFormat)),
+//      Map(
+//        "key2" ->
+//          ListValue(List(StringValue("lValue1"), StringValue("lValue2")))
+//      ).toJson(mapFormat(StringJsonFormat, sprayJsonOtherFormat))
+//    )
+    val expected1: String = """["queryValue","operationValue",{"key1":"value1"},{"key2":["lValue1","lValue2"]}]"""
+    assert(ret1.toString() == expected1)
+  }
+
+}
