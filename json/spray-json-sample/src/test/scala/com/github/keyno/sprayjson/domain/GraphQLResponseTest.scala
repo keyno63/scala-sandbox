7b18d8b9acb1fe82328ccc5dc490edffd7e6257e
Keyno <kofujiw@gmail.com>
2021-06-19 06:13:45 +0900

    fix WordSpec

--- json/spray-json-sample/src/test/scala/com/github/keyno/sprayjson/domain/GraphQLResponseTest.scala
+++ json/spray-json-sample/src/test/scala/com/github/keyno/sprayjson/domain/GraphQLResponseTest.scala
@@ -4,12 +4,15 @@ import com.github.keyno.sprayjson.domain.ExtendOther.NullValue
 import com.github.keyno.sprayjson.interop.OrigError
 import com.github.keyno.sprayjson.interop.OrigError.ExecutionError
 import com.github.keyno.sprayjson.interop.json.GraphQLResponseSprayJson
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+import org.scalatest.wordspec.AnyWordSpec
 import spray.json.{ enrichAny, JsArray, JsNull, JsObject, JsString, JsValue, RootJsonFormat }
 
-class GraphQLResponseTest extends /*AnyFunSuite*/ AnyFlatSpec {
+class GraphQLResponseTest extends AnyWordSpec {
+
+  "Request" should {
+    "hoge" in {}
+
+  }
 
   // TODO: impl in production code
   implicit object parseImpl extends RootJsonFormat[GraphQLResponse[Any]] {
@@ -21,22 +24,45 @@ class GraphQLResponseTest extends /*AnyFunSuite*/ AnyFlatSpec {
 //      assert(true)
 //    }
 //  }
-  "graphql response" should "be parsed" in {
-    val error                             = ExecutionError("first error")
-    val gqlResponse: GraphQLResponse[Any] = GraphQLResponse(ResponseValue.ListValue(List(NullValue)), List(error))
+  "graphql response" should {
+    "be able to parsed" in {
+      val error                             = ExecutionError("first error")
+      val gqlResponse: GraphQLResponse[Any] = GraphQLResponse(ResponseValue.ListValue(List(NullValue)), List(error))
 
-    val json1 = gqlResponse.toJson
-    val expected1 = JsObject(
-      Map(
-        "data" -> JsArray(JsNull),
-        "errors" -> JsArray(
-          JsObject("message" -> JsString("first error"))
+      val json1 = gqlResponse.toJson
+      val expected1 = JsObject(
+        Map(
+          "data" -> JsArray(JsNull),
+          "errors" -> JsArray(
+            JsObject("message" -> JsString("first error"))
+          )
         )
       )
-    )
-    assert(json1 == expected1)
+      assert(json1 == expected1)
 
-    val ret = json1.convertTo[GraphQLResponse[Any]]
-    assert(ret == gqlResponse)
+      val ret = json1.convertTo[GraphQLResponse[Any]]
+      assert(ret == gqlResponse)
+    }
+
+    "be able to convertTo" in {
+      val error = ExecutionError("second error")
+      val expected = GraphQLResponse(
+        ResponseValue.ListValue(List(NullValue)),
+        List(error)
+      )
+
+      val json1 = JsObject(
+        Map(
+          "data" -> JsArray(JsNull),
+          "errors" -> JsArray(
+            JsObject("message" -> JsString("second error"))
+          )
+        )
+      )
+
+      val actual = json1.convertTo[GraphQLResponse[Any]]
+      assert(actual == expected)
+
+    }
   }
 }
